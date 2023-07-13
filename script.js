5789f08ef9cbab614d655802213a56250837e08d
Marta Luba <marta.luba@yahoo.co.uk>
2023-07-13 19:23:59 +0100

    second version

--- script.js
+++ script.js
@@ -9,71 +9,12 @@
 const btn = document.querySelector(".btn");
 const ul = document.querySelector("ul");
 const close = document.querySelector(".x");
-// let id;
-// const newTask = document.createElement("li");
-// newTask.innerText = input.value;
-// list.appendChild(newTask);
-
-const addItem = function () {
-  const ul = document.querySelector("ul");
-  const input = document.querySelector("#task-title");
-  const text = input.value;
-
-  //id
-  const id = Date.now();
-
-  //li
-  // const newItem = document.createElement("li");
-  // newItem.innerText = text;
-  // newItem.dataset.id = id;
-  // newItem.onclick = () => toggleDone();
-
-  // //button
-  // const button = document.createElement("button");
-  // button.className = "x";
-  // button.innerText = "x";
-  // button.onclick = () => removeItem(id);
-  // newItem.appendChild(button);
-
-  // //li . ul
-  // ul.appendChild(newItem);
-
-  const html = `<li 
-    data-id="${id}"
-    onclick="toggleDone()">
-    ${text}
-    <button onclick="removeItem(${id})" class="x">x</button>
-  </li>`;
-
-  ul.insertAdjacentHTML("beforeend", html);
-  // ul.classList.remove("hidden");
-
-  //clean input
-  input.value = "";
-  // const html = `<li data-id="${id}">${text}<button class="x">x</button></li>`;
-  // ul.insertAdjacentHTML("beforeend", html);
-};
-
-const removeItem = function (id) {
-  const button = event.target;
-  const li = button.parentElement;
-  // const lis = document.querySelectorAll("ul li"); //data-id-unique // tablica
-  // const li = [...lis].find((el) => el.dataset.id == id);
-  // console.log(li); //dlatego [..ul] zmienione na tablice
-  li.remove();
-};
-
-const toggleDone = function () {
-  const li = event.target;
-  li.classList.toggle("done");
-};
-
-//------------------------------------------------
-btn.addEventListener("click", addItem);
-// close.addEventListener("click", removeItem);
-
 const dateLabel = document.querySelector(".date");
+const form = document.querySelector("#task-form");
+const lis = document.querySelectorAll("ul li");
+const input = document.querySelector("#task-title");
 
+//-----------------------date
 const now = new Date();
 console.log(now);
 const day = `${now.getDate()}`.padStart(2, "0");
@@ -84,3 +25,71 @@ const year = `${now.getFullYear()}`;
 console.log(year);
 
 dateLabel.textContent = `${day}/${month}/${year}`;
+
+//----------------functions
+//1.
+const addItem = function (e) {
+  // const ul = document.querySelector("ul");
+  const input = document.querySelector("#task-title");
+  const text = input.value;
+  //id
+
+  const id = Date.now();
+  console.log(id);
+
+  const html = `<li 
+    data-id="${id}"
+    class=""
+   >
+    ${text}
+    <button class="x">x</button>
+  </li>`;
+
+  ul.insertAdjacentHTML("beforeend", html);
+  //dodaj event
+  document
+    .querySelector(`[data-id="${id}"]`)
+    .addEventListener("click", toggleDone);
+
+  document
+    .querySelector(`[data-id="${id}"]`)
+    .querySelector(".x")
+    .addEventListener("click", removeItem);
+  //clean input
+  input.value = "";
+};
+
+//2.
+const removeItem = function (e) {
+  const button = e.target;
+  const li = button.parentElement;
+  li.remove();
+};
+
+//3.
+const toggleDone = function (e) {
+  const li = e.target;
+  li.classList.toggle("done");
+};
+
+//4.
+const toggleButtonDisable = function (e) {
+  const input = e.target;
+  const btn = document.querySelector(".btn");
+  btn.disabled = !input.value;
+};
+//------------------------------------------------
+//eventy
+btn.addEventListener("click", addItem);
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addItem();
+});
+
+lis.forEach((li) => {
+  li.addEventListener("click", toggleDone);
+  li.querySelector("button").addEventListener("click", removeItem);
+});
+
+input.addEventListener("keyup", toggleButtonDisable);
